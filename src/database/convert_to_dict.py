cc1cd96acc2b2b7cd5e91aff826a8a87aeefe406
shahab <oveysi.shahab@gmail.com>
2024-08-22 01:02:57 +0330

    fixing convert_to_dict type

--- src/database/convert_to_dict.py
+++ src/database/convert_to_dict.py
@@ -2,10 +2,10 @@ from typing import Dict, List, Tuple
 
 
 def convert_to_dict(
-    colnames: Tuple[str],
+    colnames: List[str] | None,
     rows: Tuple[str] | None
 ) -> List[Dict[str, str]]:
-    if rows is None:
+    if rows is None or colnames is None:
         return []
 
     data = [dict(zip(colnames, row)) for row in rows]
