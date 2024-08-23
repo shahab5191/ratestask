a0375646a48fa45588eb6c86dc0d2754662670c1
shahab <oveysi.shahab@gmail.com>
2024-08-23 14:55:17 +0330

    Implemented retrying mechanism to wait for database to start running before running integration tests

--- tests/integration/test_server.py
+++ tests/integration/test_server.py
@@ -1,3 +1,4 @@
+import time
 import pytest
 from src import create_app
 
@@ -10,10 +11,22 @@ def client():
     yield client
 
 
+def wait_for_database(client, max_retries=5, delay=5):
+    for _ in range(max_retries):
+        try:
+            response = client.get("/v1/healthz")
+            if response.status_code == 200:
+                return
+        except Exception:
+            print(f"Exeption happend trying to connect to database, retrying...")
+
+        print(f"Database not ready, retrying in {delay} seconds...")
+        time.sleep(delay)
+    raise Exception("Database not ready after waiting")
+
 def test_server_is_running(client):
-    print(client)
+    wait_for_database(client)
     response = client.get('/v1/healthz')
-    print(response)
     assert response.status_code == 200
     json_response = response.json
     assert 'status' in json_response
