7f5b14ccb277215263835185bebbdc8adae1b0f5
shahab <oveysi.shahab@gmail.com>
2024-08-24 15:33:48 +0330

    test: Integration testing less than 2 data available returns None (JSON null)

--- tests/integration/test_rates_endpoint.py
+++ tests/integration/test_rates_endpoint.py
@@ -5,6 +5,8 @@ from tests.utils.data_is_empty import is_empty
 
 
 def test_unsuccessful_empty_args(client):
+    """Test that missing query parameters return a 400 error with an error message."""
+
     response = client.get(f'{Config.API_PREFIX}/rates')
 
     assert response.status_code == 400
@@ -16,6 +18,8 @@ def test_unsuccessful_empty_args(client):
 
 
 def test_successfull_empty_data(client):
+    """Test valid parameters result in an filled rates list with null average_price when no data is available."""
+
     params = {
         "date_from": "2016-01-01",
         "date_to": "2016-01-30",
@@ -32,6 +36,8 @@ def test_successfull_empty_data(client):
 
 
 def test_successfull_available_data_port_port(client):
+    """Test valid port-to-port parameters return correct data for a specific route."""
+
     params = {
         "date_from": "2016-01-01",
         "date_to": "2016-01-02",
@@ -55,6 +61,8 @@ def test_successfull_available_data_port_port(client):
 
 
 def test_successfull_available_data_region_port(client):
+    """Test valid region-to-port parameters return correct data for a specific route."""
+
     params = {
         "date_from": "2016-01-01",
         "date_to": "2016-01-02",
@@ -78,6 +86,8 @@ def test_successfull_available_data_region_port(client):
 
 
 def test_successfull_available_data_port_region(client):
+    """Test valid port-to-region parameters return correct data for a specific route."""
+
     params = {
         "date_from": "2016-01-01",
         "date_to": "2016-01-02",
@@ -101,6 +111,8 @@ def test_successfull_available_data_port_region(client):
 
 
 def test_successfull_available_data_region_region(client):
+    """Test valid region-to-region parameters return correct data for a specific route."""
+
     params = {
         "date_from": "2016-01-01",
         "date_to": "2016-01-02",
@@ -111,6 +123,31 @@ def test_successfull_available_data_region_region(client):
     expected_result = {
         "day": "2016-01-01",
         "average_prices": "1245.70"
+    }
+
+    response = client.get(f'{Config.API_PREFIX}/rates', query_string=params)
+    result = response.json
+    assert result is not None
+    rates = result.get("rates")
+    assert rates is not None
+    assert isinstance(rates, List)
+    assert rates[0]['day'] == expected_result["day"]
+    assert rates[0]['average_price'] == expected_result["average_prices"]
+
+
+def test_successfull_available_data_less_than_two_data(client):
+    """Test valid region-to-region parameters return correct data for a specific route."""
+
+    params = {
+        "date_from": "2016-01-04",
+        "date_to": "2016-01-04",
+        "origin": "china_main",
+        "destination": "uk_main"
+    }
+
+    expected_result = {
+        "day": "2016-01-04",
+        "average_prices": None
     }
 
     response = client.get(f'{Config.API_PREFIX}/rates', query_string=params)
