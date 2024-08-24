73460f160036d32cd0ceebab46aa70877dc18e16
shahab <oveysi.shahab@gmail.com>
2024-08-24 13:23:28 +0330

    Adding new utils to document and writing docstrings for them

--- src/utils/retry_decorator.py
+++ src/utils/retry_decorator.py
@@ -4,6 +4,30 @@ from src.config import Config
 
 
 def retry(func):
+    """
+    A decorator to retry a function multiple times if it raises an exception.
+
+    This decorator retries the execution of the decorated function up to a maximum number of attempts 
+    specified by `Config.RETRY_MAX`. If an exception is raised during the execution, it logs the error 
+    and retries after a delay specified by `Config.RETRY_DELAY`. If all attempts fail, it raises an 
+    exception indicating that the function could not be executed successfully.
+
+    Args:
+        func (Callable): The function to be decorated and retried.
+
+    Returns:
+        Callable: The decorated function with retry logic.
+
+    Raises:
+        Exception: If the function fails to execute successfully after the maximum number of retries.
+
+    Example:
+        @retry
+        def connect_to_database():
+            # Function implementation here
+
+        connect_to_database()
+    """
     def wrapper(*args, **kwargs):
         for i in range(Config.RETRY_MAX):
             try:
